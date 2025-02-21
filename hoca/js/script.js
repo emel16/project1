66a6cdbd442fe7411bb014d5c88369dc8e93a2d0
emel <131045117+emel16@users.noreply.github.com>
2025-02-21 21:32:28 +0300

    Delete hoca directory

--- hoca/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let icon = document.getElementById("icon-mobile");
-    let ul = document.getElementById("ul-mobile");
-
-    icon.onclick = function () {
-        ul.classList.toggle("mobile-nav1");
-    };
-});
