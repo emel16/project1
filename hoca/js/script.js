02faf62d3b606ba7fe0007598aec30590ff54dd9
emel <131045117+emel16@users.noreply.github.com>
2025-02-21 21:31:41 +0300

    Add files via upload

---
+++ hoca/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let icon = document.getElementById("icon-mobile");
+    let ul = document.getElementById("ul-mobile");
+
+    icon.onclick = function () {
+        ul.classList.toggle("mobile-nav1");
+    };
+});
